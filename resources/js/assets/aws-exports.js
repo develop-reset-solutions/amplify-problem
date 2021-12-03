4536eb58d3eafd62cd1c121c68f80fc9c8ca9b5f
FerranPF <ferranpf98@gmail.com>
2021-12-03 16:49:32 +0100

    Google login with cognito

--- resources/js/assets/aws-exports.js
+++ resources/js/assets/aws-exports.js
@@ -1,19 +1,28 @@
 const awsconfig = {
-    "aws_project_region": "us-east-1",
-    "aws_cognito_identity_pool_id": "us-east-1:f6a3770c-c72e-4e44-bc77-f0b92c4982b3",
-    "aws_cognito_region": "us-east-1",
-    "aws_user_pools_id": "us-east-1_iLN2E6rWR",
-    "aws_user_pools_web_client_id": "7r7q1cfpj3im5a4eahkrgee7vk",
+    "aws_project_region": "us-east-2",
+    //"aws_project_region":"us-east-1",
+    "aws_cognito_identity_pool_id": "us-east-2:fbddb4e3-788d-4d81-94d9-5fd572182a92",
+    //"aws_cognito_identity_pool_id": "us-east-1:f6a3770c-c72e-4e44-bc77-f0b92c4982b3",
+    "aws_cognito_region": "us-east-2",
+    //"aws_cognito_region":"us-east-1",
+    "aws_user_pools_id": "us-east-2_MF0zNVNmz",
+    //"aws_user_pools_id": "us-east-1_iLN2E6rWR",
+    "aws_user_pools_web_client_id": "3a4a04qova5cgr65csae67mgmd",
+    //"aws_user_pools_web_client_id": "7r7q1cfpj3im5a4eahkrgee7vk",
     "oauth": {
-        "domain": "championstoken.auth.us-east-1.amazoncognito.com",
-        "scope": [
+        "domain": "resetauth.auth.us-east-2.amazoncognito.com",
+        //"domain": "championstoken.auth.us-east-1.amazoncognito.com",
+        /*"scope": [
             "phone"   , "email"   ,
             "openid"  , "profile" ,
             "aws.cognito.signin.user.admin"
-        ],
-        "redirectSignIn"  : "https://tokenchampions.com/home",
-        "redirectSignOut" : "https://tokenchampions.com/home",
-        "responseType"    : "code"
+        ],*/
+        //"redirectSignOut" : "https://tokenchampions.com/home",
+        "redirectSignOut" : "http://localhost:8000/",
+        //"redirectSignIn"  : "https://tokenchampions.com/home",
+        "redirectSignIn"  : "http://localhost:8000/",
+        //"responseType" : "code"
+        "responseType"    : "token"
     },
     "federationTarget": "COGNITO_USER_POOLS",
     "aws_cognito_login_mechanisms": [
--- resources/js/components/LogInCnt.vue
+++ resources/js/components/LogInCnt.vue
@@ -26,12 +26,30 @@ export default {
   methods:{
     async signInGoogle(){
 
-      const user = await Auth.federatedSignIn({ provider: 'Google' }).
-      then( cred => {
-        let user = Auth.currentAuthenticatedUser();
+      const user = await Auth.federatedSignIn({ provider: 'Google' }
+      ).then(cred => {
 
-        console.log( user );
-        // hay que poder conseguir este usuario
+          // If success, you will get the AWS credentials
+          console.log(cred);
+          return Auth.currentAuthenticatedUser();
+          
+      }).then(user => {
+
+          //Print the access token
+          Auth.currentSession().then(data => console.log(data.accessToken.getJwtToken()));
+          
+          /*
+          Auth.currentSession().then(res=>{
+            let accessToken = res.getAccessToken()
+            let jwt = accessToken.getJwtToken()
+            //You can print them to see the full objects
+            console.log(`myAccessToken: ${JSON.stringify(accessToken)}`)
+            console.log(`myJwt: ${jwt}`)
+          })
+          */
+
+      }).catch(e => {
+          console.log(e)
       });
 
       localStorage.setItem('user',JSON.stringify(user));
